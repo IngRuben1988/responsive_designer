930da338a740c5a65545be9f80ecc54a592fd916
Ruben Magana Alvarado <rmagana11@hotmail.com>
2021-05-06 15:35:15 -0500

    proyecto para responsive designer

---
+++ index.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="shortcut icon" href="madejas.ico">
+    <!--[if lt IE 7]>
+		<script src="css/ie7/IE9.js" type="text/javascript"></script>
+    <![endif]-->
+<title>Madejas & Puntadas</title>
+</head>
+    
+<body>
+     <!---cabeza de pagina--->
+	 <header class="tie-header">
+         <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+             <div class="tie-logo"></div>
+         </div>
+         <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+             <nav>
+                 <ul class="fullnav">
+                  <!-- <li><a  href="productos.php">Productos</a></li>
+                   <li><a href="somos.php">¿Quiénes Somos?</a></li>
+                   <li><a href="galeria.php">Saber más</a></li>
+                   <li><a class="" href="#" data-toggle="modal" data-target="#contacto">Ubicación</a></li>-->     
+                 </ul>
+             </nav>
+         
+         </div>
+     </header>
+     
+     <!---banner para información visual--->
+     <div class="tie-banner-index"></div>
+     <div class="container">
+     
+  			
+
+     </div>
+     <br>
+     
+     <!---cuerpo de la pagina--->
+     <div class="container tie-body">
+     
+     	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <center>
+            	<div class="tie-cuadro">
+                    <img class="tie-saturador" src="img/img-barda-0001.fw.png" />
+                    <br>
+                    <label class="tie-cuadro-titulo">Muestras</label>
+                    <br>
+                    <p class="tie-texto-marcos">Nosotros tenemos muestras de los alumnos que han tenido la oportunidad de estar en nuestros cursos de bordado y tejido.</p>
+                    <br>
+                    <div><button class="tie-boton-marco">Saber Mas...</button></div>
+                    <br>
+                    <br>
+                </div>
+            </center>
+        </div>
+        
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <center>
+            	<div class="tie-cuadro">
+                    <img class="tie-saturador" src="img/img-barda-0002.fw.png" />
+                    <br>
+                    <label class="tie-cuadro-titulo">Productos</label>
+                    <br>
+                    <p class="tie-texto-marcos">Los productos que manejamos en "Madejas & Puntadas" son de primer nivel y contamos con una gama muy aamplia de ellos.</p>
+                    <br>
+                    <div><button class="tie-boton-marco">Saber Mas...</button></div>
+                    <br>
+                </div>
+            </center>    
+        </div>
+        
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+            <center>
+            	<div class="tie-cuadro">
+                    <img class="tie-saturador" src="img/img-barda-0003.fw.png" />
+                    <br>
+                    <label class="tie-cuadro-titulo">Cursos</label>
+                    <br>
+                    <p class="tie-texto-marcos">Usted por cada compra, tiene la oportunidad de participar en nuestros cursos de bordado y tejido con los expertos en la materia.</p>
+                    <br>
+                    <div><button class="tie-boton-marco">Saber Mas...</button></div>
+                    <br>
+                    <br>
+                </div>
+            </center>
+        </div>
+        
+     </div>
+     
+     <!---pie de pagina--->
+     <br>
+     <footer class="tie-footer">
+     	<div class="container">
+        	<br>
+     
+         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+         			<center>
+             	    <label class="tie-footer-titulos">Nosotros</label>
+                    </center>
+                    <br/>
+                    <p class="tie-contacto-texto tie-margenes">Nosotros somos una empresa joven dedicada a la venta de articulos para el bordado y el tejido, desde 2015 estamos ubicados dentro de la Plaza Mega de Comercial Mexicana.<br>¡Recuerde que donde encuentre nuestro logo usted encontrara la mejor calidad y al mejor precio! 
+                    </p>
+                    <br>
+             		<div class="tie-tienda"></div>
+         </div>
+         
+         
+         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+         	 <center>
+             <label class="tie-footer-titulos">Contacto</label>
+             </center>
+            <br/>
+            <p class="tie-contacto-texto tie-margenes">¿Tienes una duda? Encuentra la respuesta abajo o en las preguntas frecuentes. También te atendemos por teléfono, correo electrónico, y en nuestras oficinas</p>
+                        
+            <form>
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <br>
+                    <input class="tie-contacto" type="email" placeholder="Correo."/>
+                </div>
+                
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <br>
+                    <input class="tie-contacto" type="text" placeholder="Nombre."/>
+                </div>
+              
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                    <br>
+                    <input class="tie-contacto" type="text" placeholder="Asunto."/>
+                </div>
+              
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <br>
+                    <textarea class="tie-contacto-mensaje" placeholder="Espacio para escribir lo que guste."></textarea>
+                </div>
+                
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <br>
+                  
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        <center>
+                        	<input class="tie-confirmar" type="submit"/>
+                        </center>
+                    </div>
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    	<center> 
+                        	<input class="tie-confirmar" type="reset"/>
+                        </center>
+                    </div>
+                  
+                </div>
+            </form>
+     
+         </div>
+         
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+         	<hr>
+         	 <br>
+             <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+                 <center>
+                    <label class="tie-footer-titulos">Dirección</label> 
+                 </center>
+                 <br>
+                 <p class="tie-contacto-texto tie-margenes">Gusta de visitarno? Con todo gusto le atendemos, Madejas & Puntadas se ubica
+    Atasta de Serra, Plaza Mega de Comercial Mexicana Villahermosa, Tabasco. C.P. 86100 México Tel: 01(993) 310 13 00.</p><br>
+    		 </div>
+             <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                 <center>
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1740.016465085033!2d-92.94267094877692!3d17.978605780272396!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xca07a9ee9cf43791!2sComercial+Mexicana!5e0!3m2!1ses-419!2smx!4v1463758158708" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+                 </center>
+             </div>
+         </div>
+       </div> 
+       
+     	<br><hr>
+     	<center>
+        	<p class="tie-footer-creditos">© Powered by TI-Estratégica 2016. | Todos los derechos reservados. | Use Navegadores Chrome, Opera y Safari.</p>
+            <br>
+        </center>       
+     </footer>
+
+     
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
